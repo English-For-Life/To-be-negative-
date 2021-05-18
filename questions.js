f61b3a2db318520ac6554bf9ee185205243fd025
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 12:11:10 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"It ___ hot today.",
+        options:["are not", "is not", "does not", "am not"],
+        answer: 1
+    },
+    {
+     q:"I ___ hungry.",
+        options:["am not", "are not", "is not", "do not"],
+        answer: 0   
+    },
+    {
+        q:"These dogs ___ angry.",
+        options:["do not", "am not", "is not", "are not"],
+        answer: 3
+    },
+    {
+        q:"It ___ very interesting book. I don't want to read it.",
+        options:["are not", "does not", "am not", "is not"],
+        answer: 3
+    },
+    {
+        q:"They ___ at home. They are playing football.",
+        options:["do not", "is not", "are not", "am not"],
+        answer: 2
+    },
+    {
+        q:"I ___ 29. I am 28.",
+        options:["are not", "am not", "do not", "is not"],
+        answer: 1
+    },
+    {
+        q:"We ___ friends.",
+        options:["are not", "do not", "is not", "am not"],
+        answer: 0
+    },
+    {
+        q:"It ___ very good for you!",
+        options:["are not", "am not", "does not", "is not"],
+        answer: 3
+    },
+    {
+        q:"I ___ from USA. I am from Canada.",
+        options:["do not", "am not", "is not", "are not"],
+        answer: 1
+    },
+    {
+        q:"These glasses ___ expensive.",
+        options:["is not", "am not", "are not", "do not"],
+        answer: 2
+    }
+
+]
